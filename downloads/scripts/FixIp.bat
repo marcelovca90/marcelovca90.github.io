4bb3b2364bb3afca54f49e4761acf0f4160b1942
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-04-10 10:54:31 -0300

    changed folder structure and updated links

--- downloads/scripts/FixIp.bat
+++
@@ -1,7 +0,0 @@
-call ipconfig /release
-call ipconfig /renew
-call ipconfig /flushdns
-call ipconfig /registerdns
-call nbtstat -rr
-call netsh int ip reset all
-call netsh winsock reset
--- downloads/workshops/CloneCleanInstallCompile.bat
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/neo4j-contrib/developer-resources.git
-cd .\developer-resources\language-guides\java\jdbc
-call mvn clean
-call mvn install
-call mvn compile exec:java
