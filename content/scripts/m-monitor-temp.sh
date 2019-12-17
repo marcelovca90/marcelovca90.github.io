1050771e86281981183b75131d89dca0f0c4d4d3
Marcelo Cysneiros <marcelovca90@gmail.com>
2019-12-17 18:19:10 -0300

    Update for 2020, stay tuned

--- content/scripts/m-monitor-temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# -n 1 : Run every 1 second
-# -d   : Highlight differences
-
-watch -n 1 -d sensors
-
--- tidy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . -type f -name "*.html" -exec bash -c "echo {} && cat {} | tidy -config tidy.cfg -o {}" \; > /dev/null 2>&1
