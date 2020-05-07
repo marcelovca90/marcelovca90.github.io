162abd72286034b5a11de851943a9cc1a95f32fc
Marcelo Cysneiros <marcelovca90@gmail.com>
2020-05-07 20:55:54 -0300

    Add target="_blank" to last commit href

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.getJSON("https://api.github.com/repos/marcelovca90/marcelovca90.github.io/commits/master", function(data, status){
         var commit_id = data.sha, commit_id_short = commit_id.substring(0,7);
-        var href = `<a href="https://github.com/marcelovca90/marcelovca90.github.io/commit/${commit_id}">${commit_id_short}</a>`;
+        var href = `<a href="https://github.com/marcelovca90/marcelovca90.github.io/commit/${commit_id}" target="_blank">${commit_id_short}</a>`;
         $("#last_update").append(` (${href})`);
       });
 });
